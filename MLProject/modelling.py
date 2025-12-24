2bfaedb1d353c0e389179b6cf7b555c9ca43b945
notsonnatureall <vralamsyah36@gmail.com>
2025-12-24 15:41:47 +0700

    check

---
+++ MLProject/modelling.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import mlflow
+import dagshub
+import argparse
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_estimators", type=int, default=100)
+args = parser.parse_args()
+
+dagshub.init(repo_owner='notsonnatureall', repo_name='ML-OPS', mlflow=True)
+
+def run():
+    df = pd.read_csv('financial_regression_preprocessing.csv')
+    X = df.drop(['gold high', 'Unnamed: 0'], axis=1)
+    y = df['gold high']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    with mlflow.start_run(run_name="Basic_CI_Trigger"):
+        model = RandomForestRegressor(n_estimators=args.n_estimators)
+        model.fit(X_train, y_train)
+        mlflow.sklearn.log_model(model, "model")
+        print("Training sukses!")
+
+if __name__ == "__main__":
+    run()
